7bf52ec4f90839b73a97865796f2cc78a5333424
ido-postelnik <postelnik@gmail.com>
2021-09-12 18:26:53 -0400

    [src] Install and init firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Firebase
+import { db } from './firebase-config';
+import { collection, addDoc } from "firebase/firestore"; 
+
 function App() {
+  useEffect(() => {
+    debugger;
+    (async () => {
+      debugger;
+      try {
+        const docRef = await addDoc(collection(db, "users-tests"), {
+          first: "Ada",
+          last: "Lovelace",
+          born: 1815
+        });
+        debugger;
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        debugger;
+        console.error("Error adding document: ", e);
+      }
+    })();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
