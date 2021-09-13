82dfd9069e54839d58c635283ac80035a21463a5
ido-postelnik <postelnik@gmail.com>
2021-09-12 21:46:24 -0400

    [src] register sw

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -61,6 +61,7 @@ function registerValidSW(swUrl: string, config?: Config) {
   navigator.serviceWorker
     .register(swUrl)
     .then((registration) => {
+      console.log('registerValidSW');
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         if (installingWorker == null) {
